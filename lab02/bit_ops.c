3c6aada64f46191976a0a9e0d5ea7b250358114b
WangDi <wangdi7670@163.com>
2022-08-10 12:52:52 +0800

    done lab02 exercise3 用了pow函数

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include "bit_ops.h"
+#include <math.h>
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
     /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    int temp = pow(2.0, (double) n);
+    int ans = temp & x;
+
+    return ans == temp ? 1 : 0; /* UPDATE WITH THE CORRECT RETURN VALUE*/
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
+    int temp = pow(2.0, (double) n);
+
+    if (v == 1) {
+        *x = *x | temp;
+    } else {
+        temp = ~temp;
+        *x = *x & temp;
+    }
     /* YOUR CODE HERE */
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
     /* YOUR CODE HERE */
+    int a = get_bit(*x, n);
+    a = a == 1 ? 0 : 1;
+    set_bit(x, n, a);
 }
 
